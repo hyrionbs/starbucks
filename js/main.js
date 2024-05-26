25baf394affe95103dbd28c698560ba798d6c503
hyrionbs <gorabbit2001@naver.com>
2024-05-26 23:31:35 +0900

    starbucks3day

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder','');
 });
+
+
+const badgeEl = document.querySelector('Header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
