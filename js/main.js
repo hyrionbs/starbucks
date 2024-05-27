997bc2b6a51154c45678ee02bfbe3b03ae51140d
hyrionbs <gorabbit2001@naver.com>
2024-05-27 23:37:42 +0900

    starbucks4day

--- js/main.js
+++ js/main.js
@@ -33,3 +33,11 @@ window.addEventListener('scroll', _.throttle(function () {
         });
     }
 }, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+ gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+ });
+});
