b12ababba2115047d20ce88ca7153718b9199325
hyrionbs <gorabbit2001@naver.com>
2024-06-02 09:17:55 +0900

    starbucks8-2daerry

--- js/main.js
+++ js/main.js
@@ -77,3 +77,24 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+function random(min, max) {
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+    gsap.to
+        (selector, 
+        random(1.5, 2.5), 
+        {
+        y: 20,
+        repeat:-1,
+        yoyo: true,
+        ease: "power1.inOut",
+        delay:random(0, delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
