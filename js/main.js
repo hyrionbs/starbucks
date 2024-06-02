5cf5962975536a61bb83759cf22e98f1b912a016
hyrionbs <gorabbit2001@naver.com>
2024-06-03 01:51:06 +0900

    starbucks9day

--- js/main.js
+++ js/main.js
@@ -98,3 +98,14 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+    .Scene({
+        triggerElement: spyEl,
+        triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
